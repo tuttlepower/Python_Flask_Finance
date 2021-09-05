c4a9c69aeb67b93fa824e1b3816a7117ccff9b5e
Tuttlepower <tatuttle2015@gmail.com>
2021-09-05 12:01:36 -0400

    Attempt at a readme + favicon

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Python_Flask_Finance
+
+![alt text](static\images\placeholder_logo.png?raw=true "Title")
+
+Simple project for personal finances utilizing Flask, Plaids api, and eventually Google log-in funcitonality. 
+Would like to add React front end components as well. 
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, session, redirect, url_for, request, jsonify
-
+from flask import Flask, render_template, session, redirect, url_for, request, jsonify,send_from_directory
+import os
 app = Flask(__name__)
 
 
@@ -12,6 +12,10 @@ def home():
 def test():
     return render_template("test.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+                               'favicon.ico', mimetype='image/png')
 
 if __name__ == "__main__":
     app.run(debug=True)
