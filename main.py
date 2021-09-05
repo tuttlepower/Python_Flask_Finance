ef01d7d9f3934873b5a1371406d898c6683c1587
Tuttlepower <tatuttle2015@gmail.com>
2021-09-05 11:19:58 -0400

    Taking out Stock data
    Will probably move this old code to a different repo. Going to repurpose this one

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, session, redirect, url_for, request, jsonify
 import pandas as pd
-import StockDAO as DAO
-import Stock as Stock
 
 
 app = Flask(__name__)
@@ -12,45 +10,9 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/get_piechart_data/<ticker>/<timePeriod>')
-def get_piechart_data(ticker = 'tsla', timePeriod ='1m'):
-    # df = DAO.get_yf_stock_price(ticker, timePeriod)
-    # stock = pd.DataFrame(DAO.createStock(df)) 
-    df = DAO.get_yf_stock_price(ticker, timePeriod)
-    print(df.head())
-
-    return jsonify(str(df.to_dict()))
-
-
-@app.route("/about")
-def about():
-    return '<h1>This is the way</h1>'
-
-
 @app.route("/test", methods=['GET', 'POST'])
 def test():
-    if request.method == 'POST':
-        ticker = request.form['ticker']
-        timePeriod = request.form['time']
-        df = DAO.get_yf_stock_price(ticker, timePeriod)
-        stock = pd.DataFrame(DAO.createStock(df))
-        return render_template("test.html", df=df)
-
     return render_template("test.html")
-
-
-@app.route("/chartJSTest", methods=['GET', 'POST'])
-def chartJSTest():
-    if request.method == 'POST':
-        ticker = request.form['ticker']
-        timePeriod = request.form['time']
-        # proof of concept. The Post is working
-        print(ticker)
-        print(timePeriod)
-        df = DAO.get_yf_stock_price(ticker, timePeriod)
-        stock = pd.DataFrame(DAO.createStock(df))
-        return render_template("chartJSTest.html", ticker=ticker, timePeriod=timePeriod, opens= df['Open'])
-    return render_template("chartJSTest.html")
 
 
 if __name__ == "__main__":
