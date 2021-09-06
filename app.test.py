6dc3f0f6db37b561b35115b353d2467e6484197d
Tuttlepower <tatuttle2015@gmail.com>
2021-09-06 15:25:19 -0400

    Testing init

---
+++ app.test.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_other(self):
+        tester = app.test_client(self)
+        response = tester.get('a', content_type='html/text')
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(b'does not exist' in response.data)
+
+if __name__ == '__main__':
+    unittest.main()
